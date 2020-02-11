02b3d9e76fc6395cd1fdbf30fedb762bedfa2cc0
jxvisn <jxvisn@gmail.com>
2020-02-10 20:17:00 -0800

    Built the base program.

--- Program.cs
+++ Program.cs
@@ -10,6 +10,38 @@ namespace NumberOfSteps
     {
         static void Main(string[] args)
         {
+
+            //First we need some Input
+            Console.WriteLine("Give us an input: ");
+            var input = Convert.ToInt32(Console.ReadLine());
+            var originalInput = input;
+            //Then we need to evaluate that input
+
+            var numberOfSteps = 0; // We need to keep track of the number of steps it takes to solve
+
+            while (input != 0)
+            {
+
+                if(input % 2 == 0)
+                {
+                    // Then the number is an even number and we need to divide by two
+                    input = input / 2;
+                    numberOfSteps++;
+
+                } else if (input % 2 != 0)
+                {
+                    // Then this must be an odd number, subtract one to make it even
+                    input = input - 1;
+                    numberOfSteps++;
+
+                }
+
+            }
+            //Number is equal to 0 we are done evaluating
+            Console.WriteLine("Your Original Input Was: " + originalInput);
+            Console.WriteLine("Number of steps to solve: " + numberOfSteps);
+            //Then we need to give the output
+
         }
     }
 }
